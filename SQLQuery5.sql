ae191c972a72d6f491e482e4fd7bd4b22efedc3d
sangoladeeniola-web <sangoladeeniola@gmail.com>
2026-05-24 20:32:44 +0100

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,8 @@
+--SELECT Gender, COUNT(Gender) AS CountGender
+--FROM EmployeeDemographics
+--WHERE Age > 31
+--GROUP BY Gender
+--ORDER BY Gender
+SELECT *
+FROM EmployeeDemographics
+ORDER BY 4 DESC, 5 Desc
---
+++ SQLQuery7.sql
@@ -0,0 +1,4 @@
+SELECT TOP (1000) [EmployeeID]
+      ,[JobTitle]
+      ,[Salary]
+  FROM [SQL].[dbo].[EmployeeSalary]
